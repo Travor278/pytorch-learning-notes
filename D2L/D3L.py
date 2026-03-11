d61732573ea9ae8a98a6ad26d474bb48f3d195e3
Travor <3488616445@qq.com>
2026-03-11 11:36:24 -0500

    update D2L/D3L.py

--- D2L/D3L.py
+++ D2L/D3L.py
@@ -38,3 +38,27 @@ x, x.sum(), x.sum().item() # item()将单元素张量转换为Python数值
 A.mean(), A.sum() / A.numel() # numel()返回张量中的元素数量
 # %%
 A.mean(axis=0), A.sum(axis=0) / A.shape[0]
+# %%
+sum_A = A.sum(axis=1, keepdim=True) # keepdim=True保持原有维度
+sum_A, sum_A.shape
+# %%
+A / sum_A
+# %%
+A.cumsum(axis=0) # 沿着轴0累积求和
+# %%
+A.cumsum(axis=1) # 沿着轴1累积求和
+# %%
+y = torch.ones(4, dtype=torch.float32)
+x, y, torch.dot(x, y), x.sum() # dot()计算两个张量的点积
+# %%
+torch.norm(x) # norm()计算张量的范数
+# %%
+A.shape, x.shape, torch.mv(A, x) # mv()计算矩阵和向量的乘积
+# %%
+B = torch.ones(4, 3)
+A.shape, B.shape, torch.mm(A, B) # mm()计算矩阵和矩阵的乘积
+# %%
+u = torch.tensor([-3.0, 4.0])
+torch.norm(u), torch.sqrt((u**2).sum()) # 计算向量的范数
+# %%
+torch.abs(u), torch.abs(u).sum() # 计算向量的L1范数
